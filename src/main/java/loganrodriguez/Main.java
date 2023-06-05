8d1546057f5083d324316866db1ce3281b19b1ce
Logan Rodriguez <Rodriguezlogan0428@gmail.com>
2023-06-05 16:51:55 -0500

    maven

--- src/main/java/loganrodriguez/Main.java
+++ src/main/java/loganrodriguez/Main.java
@@ -1,5 +1,12 @@
-package loganrodriguez;public class Main {
+package loganrodriguez;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String str = "Life, The Universe, and Everything";
+
+        System.out.println("You entered: \"" + str + "\"");
+        if (StringUtils)
+        System.out.println("\"" + str + "\" is a number" );
     }
 }
